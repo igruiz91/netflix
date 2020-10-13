4b08955b940ed5dcb80c8f1ec9a3459b42ce3eec
gonisr <israel.gonzalez@padigitale.it>
2020-10-14 01:47:55 +0200

    7:13 del tutorial

---
+++ src/components/card/styles/card.js
@@ -0,0 +1,95 @@
+import styled from "styled-components/macro";
+
+export const Title = styled.p`
+  font-size: 24px;
+  color: #e5e5e5;
+  font-weight: bold;
+  margin-left: 56px;
+  margin-right:56px; 
+  margin-top: 0;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+  box-sizing: border-box;
+
+  >${Title} {
+    @media (max-width: 1000px){
+      margin-left: 30px
+    }
+  }
+  &:last-of-type{
+    margin-bottom: 0;
+  }
+`;
+export const Group = styled.div`
+  display: flex;
+  flex-direction: ${({flexDirection}) => flexDirection ==='row' ? 'row' : 'column'};
+  ${(alignItems) => alignItems && `align-items: ${alignItems}`};
+  ${({margin}) => margin && `margin: ${margin}`};
+
+  > ${Container}:first-of-type{
+    @media (min-width: 1100px){
+      margin-top: -150px;
+    }
+  }
+`;
+export const SubTitle = styled.p``;
+export const Text = styled.p``;
+export const Feature = styled.div``;
+export const FeatureTitle = styled(Title)`
+margin-left: 0;
+`;
+export const FeatureText = styled.p``;
+export const FeatureClose = styled.button`
+  color: white;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  cursor: pointer;
+  background-color: transparent;
+  border: 0;
+
+  img {
+    filter: brightness(0) invert(1);
+    width: 24px;
+  }
+`;
+export const Maturity = styled.div`
+  background-color: ${({rating}) => (rating>14 ? 'red' : 'green')};
+  border-radius: 15px;
+  width: 20px;
+  padding: 5px;
+  font-size: 12px;
+  text-align: center;
+  font-weight: bold;
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.2);
+  margin-right: 10px;
+
+`;
+export const Content = styled.div``;
+export const Meta = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+  background-color: #00000f;
+`;
+export const Entities = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const Item = styled.div``;
+export const Image = styled.img`
+  border: 0; 
+  width: 100%;
+  max-width: 305px;
+  cursor: pointer;
+  height: auto;
+  padding: 0;
+  margin: 0;
+
+`;
